5791df92b42b6b1d31d64f1a4b24aee87d5e72ed
Manjarres <nicolasmanjarresg@gmail.com>
2026-07-01 12:51:48 -0500

    first commit

---
+++ src/main/java/com/devsenior/nmanja/mechply/MechplyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.devsenior.nmanja.mechply;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MechplyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MechplyBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsenior/nmanja/mechply/MechplyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsenior.nmanja.mechply;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MechplyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
